90615b535775402735c8ad0daf1707e16ab3c6f9
xuzhikang <531433607@qq.com>
2020-05-12 23:42:20 +0800

    测试

--- src/page/Home/index.vue
+++ src/page/Home/index.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="home">
-  <van-button plain type="primary">朴素按钮</van-button>
+    <div class="home-header">
+      <van-button class="home-header-button" plain size="small" type="primary">退出</van-button>
+    </div>
   </div>
 </template>
 
 <script>
-  import { Button } from 'vant';
-  export default{
-    components:{
-      [Button.name]:Button
+  import {
+    Button
+  } from 'vant';
+  export default {
+    components: {
+      [Button.name]: Button
     }
   }
 </script>
 
 <style lang="less" scoped>
+  .home {
+    .home-header {
+      display: flex;
+      justify-content: flex-end;
+      padding: .3rem .3rem 0 0;
+      .home-header-button {
+        border-radius: .2rem;
+      }
+    }
+  }
 </style>
